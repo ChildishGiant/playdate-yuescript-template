a0bef300d5c570b49643f52d282d39734eec7331
Allie <13716824+ChildishGiant@users.noreply.github.com>
2022-04-23 10:53:30 +0100

    update to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/env sh
+
+# Wipe compiled lua
+rm ./source/*.lua -f # -f to surpress output
+# Compile yue to lua
 yue ./source/yue -t ./source/
-echo "Yue compiled"
 # Add imports
 echo 'import "CoreLibs/object"
 import "CoreLibs/graphics"
@@ -8,6 +12,5 @@ import "CoreLibs/timer"
 import "CoreLibs/crank"' > tmpfile && cat source/main.lua >> tmpfile && mv tmpfile source/main.lua
 # Replace requires with imports
 find source/*.lua -exec sed -i 's/require/import/g' {} +
-echo "Imports added"
 # Compile pdx
-pdc ./source build
+pdc --skip-unknown --quiet ./source build  # Don't copy source files to build

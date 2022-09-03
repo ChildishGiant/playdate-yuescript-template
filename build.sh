88f39a9ab1545b93b1618429e9b88f27b3df2da6
Allie <13716824+ChildishGiant@users.noreply.github.com>
2022-09-03 22:08:43 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,6 @@
 find ./source/ -type f -name "*.lua" -exec rm {} +
 # Compile yue to lua
 yue ./source/yue -t ./source/
-# Add imports
-echo 'import "CoreLibs/object"
-import "CoreLibs/graphics"
-import "CoreLibs/sprites"
-import "CoreLibs/timer"
-import "CoreLibs/crank"' > tmpfile && cat source/main.lua >> tmpfile && mv tmpfile source/main.lua
 # Replace requires with imports
 find source/ -type f -name "*.lua" -exec sed -i 's/require/import/g' {} +
 # Compile pdx
